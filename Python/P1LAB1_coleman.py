2b61f4b9151be010f6c96e7b59442c452338ae38
jtcoleman123 <jtcoleman123@gmail.com>
2024-06-18 12:36:58 -0400

    Add files via upload

---
+++ Python/P1LAB1_coleman.py
@@ -0,0 +1,14 @@
+# JT Coleman
+# 6/18/24
+# P1LAB1
+# Create a first and last name input and insert it into a response
+
+print('Enter your first name:')
+first_name = input()
+
+print('Enter your last name:')
+last_name = input()
+
+print('Hello, ' + first_name + ' ' + last_name + '! Welcome to CTI-110')
+print()
+input('Press ENTER to exit...')
